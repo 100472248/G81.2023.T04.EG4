3111121ef8fe8c80301a3b71d37bc2134cb32ae2
100472173 <100472173@alumnos.uc3m.es>
2023-04-26 09:26:59 +0200

    Singleton json_order_store

--- src/main/python/uc3m_logistics/json_store/json_order_store.py
+++ src/main/python/uc3m_logistics/json_store/json_order_store.py
@@ -4,20 +4,34 @@ from uc3m_logistics.config.order_management_exception import OrderManagementExce
 from uc3m_logistics.json_store.json_store_master import JsonStoreMaster
 
 
-class JsonOrderStore(JsonStoreMaster):
-    """Subclass used for the orders"""
+class JsonOrderStore:
+    class __JsonOrderStore(JsonStoreMaster):
+        """Subclass used for the orders"""
 
-    def __init__(self):
-        self._file_path = JSON_FILES_PATH + "orders_store.json"
-        self._data_list = []
-        self._id_field = "_OrderRequest__order_id"
+        def __init__(self):
+            self._file_path = JSON_FILES_PATH + "orders_store.json"
+            self._data_list = []
+            self._id_field = "_OrderRequest__order_id"
 
-    def add_item(self, item):
-        """Only if it isn´t exist in the file"""
-        self.load_store()
-        found = self.find_data(item.order_id)
-        if found is None:
-            self._data_list.append(item.__dict__)
-        else:
-            raise OrderManagementException("order_id is already registered in orders_store")
-        self.save_store()
+        def add_item(self, item):
+            """Only if it isn´t exist in the file"""
+            self.load_store()
+            found = self.find_data(item.order_id)
+            if found is None:
+                self._data_list.append(item.__dict__)
+            else:
+                raise OrderManagementException("order_id is already registered in orders_store")
+            self.save_store()
+
+    instance = None
+
+    def __new__(cls):
+        if not JsonOrderStore.instance:
+            JsonOrderStore.instance = JsonOrderStore.__JsonOrderStore()
+        return JsonOrderStore.instance
+
+    def __getattr__(self, nombre):
+        return getattr(self.instance, nombre)
+
+    def __setattr__(self, nombre, valor):
+        return setattr(self.instance, nombre, valor)
--- src/unittest/python/test_singleton_tests.py
+++ src/unittest/python/test_singleton_tests.py
@@ -1,7 +1,7 @@
 """Module for testing singleton"""
 from unittest import TestCase
 from uc3m_logistics import OrderManager
-
+from uc3m_logistics.json_store.json_order_store import JsonOrderStore
 
 class TestSigleton(TestCase):
     """Tests of singleton"""
@@ -14,3 +14,12 @@ class TestSigleton(TestCase):
         self.assertEqual(om1, om2)
         self.assertEqual(om2, om3)
         self.assertEqual(om1, om3)
+
+    def test_singleton_json_order_store(self):
+        """Test if numerous JsonOrderStore are equal"""
+        os1 = JsonOrderStore()
+        os2 = JsonOrderStore()
+        os3 = JsonOrderStore()
+        self.assertEqual(os1, os2)
+        self.assertEqual(os2, os3)
+        self.assertEqual(os1, os3)
