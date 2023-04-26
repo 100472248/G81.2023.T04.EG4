b505e58267ee1574423daa1fff9f141a42256f31
100472248 <100472248@alumnos.uc3m.es>
2023-04-26 09:18:27 +0200

    singleton order_manager

--- src/main/python/uc3m_logistics/order_manager.py
+++ src/main/python/uc3m_logistics/order_manager.py
@@ -8,40 +8,54 @@ from uc3m_logistics.order_data.order_delivered import OrderDelivered
 
 
 class OrderManager:
-    """Class for providing the methods for managing the orders process"""
+    class __OrderManager:
+        """Class for providing the methods for managing the orders process"""
 
-    def __init__(self):
-        pass
+        def __init__(self):
+            pass
 
-    # pylint: disable=too-many-arguments
-    @staticmethod
-    def register_order(product_id, order_type,
-                       address, phone_number, zip_code):
-        """Register the orders into the order's file"""
-        my_order = OrderRequest(product_id, order_type, address,
-                                phone_number, zip_code)
-        my_store = JsonOrderStore()
-        my_store.add_item(my_order)
-        return my_order.order_id
+        # pylint: disable=too-many-arguments
+        @staticmethod
+        def register_order(product_id, order_type,
+                           address, phone_number, zip_code):
+            """Register the orders into the order's file"""
+            my_order = OrderRequest(product_id, order_type, address,
+                                    phone_number, zip_code)
+            my_store = JsonOrderStore()
+            my_store.add_item(my_order)
+            return my_order.order_id
 
-    # pylint: disable=too-many-locals
-    @staticmethod
-    def send_product(input_file):
-        """Sends the order included in the input_file"""
-        try:
-            # check all the information
-            my_sign = OrderShipping(input_file)
-        except KeyError as ex:
-            raise OrderManagementException("Bad label") from ex
-        # save the OrderShipping in shipments_store.json
-        my_store = JsonDeliverStore()
-        my_store.save_orders_shipped(my_sign)
-        return my_sign.tracking_code
+        # pylint: disable=too-many-locals
+        @staticmethod
+        def send_product(input_file):
+            """Sends the order included in the input_file"""
+            try:
+                # check all the information
+                my_sign = OrderShipping(input_file)
+            except KeyError as ex:
+                raise OrderManagementException("Bad label") from ex
+            # save the OrderShipping in shipments_store.json
+            my_store = JsonDeliverStore()
+            my_store.save_orders_shipped(my_sign)
+            return my_sign.tracking_code
 
-    @staticmethod
-    def deliver_product(tracking_code):
-        """Register the delivery of the product"""
-        my_deliver = OrderDelivered(tracking_code)
-        del_timestamp = my_deliver.check_tracking_code()
-        my_deliver.check_date(del_timestamp)
-        return True
+        @staticmethod
+        def deliver_product(tracking_code):
+            """Register the delivery of the product"""
+            my_deliver = OrderDelivered(tracking_code)
+            del_timestamp = my_deliver.check_tracking_code()
+            my_deliver.check_date(del_timestamp)
+            return True
+
+    instance = None
+
+    def __new__(cls):
+        if not OrderManager.instance:
+            OrderManager.instance = OrderManager.__OrderManager()
+        return OrderManager.instance
+
+    def __getattr__(self, nombre):
+        return getattr(self.instance, nombre)
+
+    def __setattr__(self, nombre, valor):
+        return setattr(self.instance, nombre, valor)
